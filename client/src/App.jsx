3274ba3d9b386960417012b4791ad5e723874026
rajputanuj303 <rajputanuj303@gmail.com>
2026-03-26 01:47:52 +0530

    Project Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,26 +5,33 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
 import AuthContext from "../context/AuthContext";
+import assets from "./assets/assets";
 
 const App = () => {
   const { authUser } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
-      <Toaster />
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
+    <div
+      className="bg-cover bg-center h-screen bg-no-repeat w-full relative"
+      style={{ backgroundImage: `url(${assets.bgImage})` }}
+    >
+      <div className="absolute inset-0 backdrop-blur-sm z-0"></div>
+      <div className="relative z-10 h-full">
+        <Toaster />
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,9 +9,9 @@ const HomePage = () => {
   const { selectedUser } = useContext(ChatContext);
 
   return (
-    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+    <div className="w-full h-screen px-4 py-4 sm:px-[5%] sm:py-[5%] lg:px-[10%] xl:px-[15%] flex items-center justify-center">
       <div
-        className={`backdrop-blur-x1 border-2 border-gray-600 rounded-2x1 overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] x1:grid-cols-[1fr_2fr_1fr]" : "md: grid-cols-2"}`}
+        className={`w-full max-w-[1200px] bg-white/10 backdrop-blur-xl border-2 border-gray-600 rounded-3xl overflow-hidden h-full sm:h-[90%] grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] lg:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-[1fr_2fr]"} shadow-2xl`}
       >
         <Sidebar />
         <ChatContainer />
